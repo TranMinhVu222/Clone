17551aa0180b98b5013d968551c5d95086b89826
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2023-06-26 17:28:59 +0700

    Revert "[VuTM] REWORK: Rename object ; Creat root object in PopupManager"
    This reverts commit f95d605788c82f1375645be32f0cc4d2338a9aec.

---
+++ Assets/Script/Data/MediumOfExchange.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediumOfExchange
+{
+    public Sprite image;
+    public ulong price;
+}
+public class AngryCoin : MediumOfExchange
+{
+          
+}
+
+public class Gem: MediumOfExchange
+{
+        
+}
